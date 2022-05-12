c378806c4f6d258c5891fc49ffe20250393a20c7
danhhuynh <danhht@vng.com.vn>
2022-05-12 10:45:08 +0700

    new code

--- builder/loansInfoDrawing.js
+++ builder/loansInfoDrawing.js
@@ -50,7 +50,7 @@ export default class LoansInfoDrawing extends Builder {
     switch (this.loanInfo["scheme"]) {
       case "Employee Cash Loan":
         this.draw("X", x, y);
-        this.draw(this.loanInfo["scheme_detail"], x + 130, y, 7);
+        this.draw(this.loanInfo["scheme_detail"], x + 120, y, 7);
         break;
       case "Self-Employee":
         this.draw("X", x + 232, y);
