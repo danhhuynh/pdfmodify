b3c795c09b9694b855e987259efd5ed8f3697cf2
danhhuynh <danhhuynh2709@gmail.com>
2022-07-14 21:32:31 +0700

    MC - add LeadMC model

---
+++ models/LeadMC.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+var Schema = mongoose.Schema;
+mongoose.pluralize(null);
+import "./customer";
+
+var MySchema = new Schema(
+  {
+    locked: Boolean,
+    customer: { type: Schema.Types.ObjectId, ref: "Customer" },
+  },
+  {
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+    strict: false,
+  }
+);
+
+export default mongoose.model("LeadMC", MySchema);
