eadd6f90e263ca28e1f792a8c31a981ae59db41b
danhhuynh <niper2012@gmail.com>
2022-08-26 10:24:54 +0700

    Update Section_2_UI.js

--- Mcredit/CoordinateUI/Section_2_UI.js
+++ Mcredit/CoordinateUI/Section_2_UI.js
@@ -108,17 +108,17 @@ export default function genArrPoint(data) {
       y: y - 9 + 545,
     },
     {
-      text: data["contract_credit_date_end"].split("/")[0],
+      text: data["contract_credit_date_end"] && data["contract_credit_date_end"].split("/")[0],
       x: 405,
       y: y - 9 + 530,
     },
     {
-      text: data["contract_credit_date_end"].split("/")[1],
+      text: data["contract_credit_date_end"] && data["contract_credit_date_end"].split("/")[1],
       x: 425,
       y: y - 9 + 530,
     },
     {
-      text: data["contract_credit_date_end"].split("/")[2],
+      text: data["contract_credit_date_end"] && data["contract_credit_date_end"].split("/")[2],
       x: 450,
       y: y - 9 + 530,
     },
